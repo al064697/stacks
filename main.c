03cad3d485e52f4b1e718fe41e6230e02b250e45
al064697 <al064697@uacam.mx>
2024-04-14 10:24:05 -0600

    update comments

--- main.c
+++ main.c
@@ -1,138 +1,143 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//!Estructuras dinámicas: PILAS
+//!Dynamic structures: STACKS
 
-//1.- Crear una pila
-typedef int Item; // Definimos Item como un alias para int
+//1.- Create a stack
+typedef int Item; 
 
-// Definimos una estructura para los nodos de la pila
 typedef struct node {
-    Item element; // El elemento que se guarda en el nodo
-    struct node *next; // Un puntero al siguiente nodo en la pila
+    Item element; 
+    struct node *next;
 }Node;
 
-// Definimos una estructura para la pila
 typedef struct {
-    Node *top; // Un puntero al nodo superior de la pila
+    Node *top;
 }Stack;
 
-// Función para crear una pila
 void createStack(Stack *stack) {
-    stack->top = NULL; // Inicializamos la pila con NULL, indicando que la pila está vacía
+    stack->top = NULL;
 }
 
-// Función para verificar si la pila está vacía
 int isStackEmpty(Stack stack) {
-    return (stack.top == NULL); // Si el top es NULL, la pila está vacía
-    //1.- Pila vacía
-    //0.- Pila con elementos
+    return (stack.top == NULL);
+    //1.- Stack is empty
+    //0.- Stack has elements
 }
 
-/*2.- Retorna el primer elemento de la pila*/
+
+/*2.- Returns the top element of the stack*/
 Item top(Stack *stack) {
-    if(isStackEmpty(*stack)) { // Si la pila está vacía, imprime "Underflow" y termina el programa
+    if(isStackEmpty(*stack)) {
         puts("Underflow");
         exit(1);
     }
-    return stack->top->element; // Si la pila no está vacía, retorna el elemento en el top de la pila
+    return stack->top->element;
 }
 
-//3.- Insertar un elemento a la pila
+
+//3.- Insert an element into the stack
+
 void insert(Stack *stack, Item element) {
-    Node *newNode; // Creamos un nuevo nodo
-    newNode = (Node*) malloc(sizeof(Node)); // Asignamos memoria para el nuevo nodo
-    newNode -> element = element; // Asignamos el elemento al nuevo nodo
-    newNode -> next = stack->top; // El siguiente nodo después del nuevo nodo es el actual top de la pila
-    stack->top = newNode; // El nuevo nodo se convierte en el top de la pila
+    Node *newNode;
+    newNode = (Node*) malloc(sizeof(Node));
+    newNode -> element = element;
+    newNode -> next = stack->top;
+    stack->top = newNode;
 }
 
-//4.- Eliminar un elemento de la pila
+
+//4.- Remove an element from the stack
+
 void removeFirstNode(Stack *stack) {
-    if(isStackEmpty(*stack) == 0) { // Si la pila no está vacía
+    if(isStackEmpty(*stack) == 0) {
         Node *f;
-        f=stack->top; // Guardamos el nodo superior en f
-        stack->top=f->next; // El segundo nodo se convierte en el top de la pila
-        free(f); // Liberamos la memoria del nodo superior original
-        printf("El elemento superior ha sido eliminado.\n");
-    }else printf("La pila está vacía.\n");
+        f=stack->top;
+        stack->top=f->next;
+        free(f);
+        printf("The top element is removed.\n");
+
+    }else printf("The stack is empty.\n");
+
 }
 
-//5.- Limpiar la pila
+//5.-Clean the stack
 void cleanStack(Stack *stack) {
-    while(!isStackEmpty(*stack)) { // Mientras la pila no esté vacía
-        removeFirstNode(stack); // Eliminamos el nodo superior
+    while(!isStackEmpty(*stack)) {
+        removeFirstNode(stack);
     }
 }
 
-//6.- Contar el número de elementos en la pila
+
+//6.- Count the number of elements in the stack
 int sizeStack(Stack stack) {
     Node *f;
     int count = 0;
-    for(f=stack.top; f!=NULL; f=f->next) { // Recorremos todos los nodos de la pila
-        count++; // Incrementamos el contador por cada nodo
+    for(f=stack.top; f!=NULL; f=f->next) {
+        count++;
     }
-    return count; // Retornamos el contador
+    return count;
 }
 
-// Función del menú
+
+//Menu function
 int menu() {
-    system("clear"); // Limpiamos la consola
+    system("clear");
     int option;
-    printf("********** PILAS **********\n"
-    "\n1.- Crear una pila."
-    "\n2.- Insertar un elemento en la pila."
-    "\n3.- Eliminar un elemento de la pila."
-    "\n4.- Tamaño de la pila."
-    "\n5.- Limpiar la pila."
-    "\n6.- Elemento superior de la pila."
-    "\n0.- Salir."
-    "\n\nElige una opción: "); 
-    scanf("%d", &option); // Leemos la opción del usuario
-    return option; // Retornamos la opción
+    printf("********** STACKS **********\n"
+    "\n1.- Create a stack."
+    "\n2.- Insert an element into the stack."
+    "\n3.- Remove an element from the stack."
+    "\n4.- Stack size."
+    "\n5.- Clean the stack."
+    "\n6.- Top element of the stack."
+    "\n0.- Exit."
+    "\n\nChoose an option: "); 
+    scanf("%d", &option);
+    return option;
 }
 
 int main() {
-    Stack stack; // Creamos una pila
+    Stack stack;
     int option, n;
-    option = menu(); // Mostramos el menú y obtenemos la opción del usuario
-    while (option!=0) { // Mientras la opción no sea 0 (salir)
-        switch(option) { // Realizamos una acción diferente dependiendo de la opción
+    option = menu();
+    while (option!=0) {
+        switch(option) {
             case 1:
-                createStack(&stack); // Creamos una pila
-                printf("\nPila creada.");
+                createStack(&stack);
+                print("\nStack created.");
                 getchar();
                 break;
             case 2: 
-                printf("\nInsertar un elemento: ");
-                scanf("%d", &n); // Leemos el elemento del usuario
-                insert(&stack, n); // Insertamos el elemento en la pila
-                printf("\nElemento insertado.");
+                printf("\nInsert an element: ");
+                scanf("%d", &n);
+                insert(&stack, n);
+                printf("\nElement inserted.");
                 getchar();
                 break;
             case 3:
-                removeFirstNode(&stack); // Eliminamos el elemento superior de la pila
+                removeFirstNode(&stack);
                 getchar();
                 break;
             case 4:
-                printf("\nTamaño de la pila: %d", sizeStack(stack)); // Imprimimos el tamaño de la pila
+                printf("\nStack size: %d", sizeStack(stack));
                 getchar();
                 break;
             case 5:
-                cleanStack(&stack); // Limpiamos la pila
-                printf("\nPila limpiada.");
+                cleanStack(&stack);
+                printf("\nStack cleaned.");
                 getchar();
                 break;
             case 6:
-                printf("\nElemento superior: %d", top(&stack)); // Imprimimos el elemento superior de la pila
+                printf("\nTop element: %d", top(&stack));
                 getchar();
                 break;
             default:
-                printf("\nSaliendo...");
-                system("exit"); // Salimos del programa
+                printf("\nExiting...");
+                system("exit");
                 break;
         }
-        option = menu(); // Mostramos el menú de nuevo y obtenemos la nueva opción del usuario
+        option = menu();
     } 
-    return 0; // Terminamos el programa
+    return 0;
 }
